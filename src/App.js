1b67b6ef353188e3c660a311c1dc360851dfc385
Alexandre <pedrozaalexandre@gmail.com>
2021-02-16 23:16:47 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { entries } from "./game-data.json";
+import "./App.css";
 
 function App() {
+  function gameItem(game) {
+    return (
+      <li>
+        {game.name}, approximately {game.reviews_total * 50} units sold.{" "}
+        <a
+          href={"https://store.steampowered.com/app/".concat(game.steam_appid)}
+          target="blank"
+        >
+          Link.
+        </a>
+      </li>
+    );
+  }
+
+  let counter = 0;
+  const filteredGameData = entries.filter((entry) => {
+    if (
+      counter < 50 &&
+      (entry.steam_release_date.startsWith("2020") ||
+        entry.steam_release_date.startsWith("2021"))
+    ) {
+      counter++;
+      return true;
+    } else return false;
+  });
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        50 best-selling indie games released since january 2020
       </header>
+      <p className="App-data">
+        <ol>{filteredGameData.map((entry) => gameItem(entry))}</ol>
+      </p>
     </div>
   );
 }
